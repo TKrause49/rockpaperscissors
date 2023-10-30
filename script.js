284f34a9125af5d7322543ff7c9cd436b1b9534e
Trey Krause <tjkrause49@gmail.com>
2023-10-30 09:45:59 -0400

    Add GUI and score tracking
    Create unstyled UI for user to play against computer. Also add two divs
    that keep track of the score and reset after player or computer reaches
    5 wins.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-let choice = Math.floor(Math.random()*3+1);
+    let choice = Math.floor(Math.random()*3+1);
 
     if (choice === 1){
         return 'rock';
@@ -10,31 +10,60 @@ let choice = Math.floor(Math.random()*3+1);
     }
 };
 
-function playRound(playerChoice, computer) {
-    let player = playerChoice.toLowerCase();
-    // console.log(player);
-    // console.log(computer);
+const results = document.querySelector('#results');
+const computerPoint = document.querySelector('#computerPoint');
+const playerPoint = document.querySelector('#playerPoint');
+let playerScore = 0;
+let computerScore = 0;
 
-    return player === 'rock' && computer === 'paper' ? 'You lose!' :
-    player === 'rock' && computer === 'scissors' ? 'You WIN!' :
-    player === 'scissors' && computer === 'rock' ? 'You lose!' :
-    player === 'scissors' && computer === 'paper' ? 'You WIN!' :
-    player === 'paper' && computer === 'scissors' ? 'You lose!' :
-    player === 'paper' && computer === 'rock' ? 'You WIN!' :
-    player === computer ? 'Draw!' : 'Please choose a valid weapon.';
-}
+function playRound(player, computer) {
+    // let player = playerChoice.toLowerCase();
 
-// console.log(playRound(prompt('Choose your weapon:'), getComputerChoice()));
+    player === 'rock' && computer === 'paper' ? results.textContent = 'You lose!' :
+    player === 'rock' && computer === 'scissors' ? results.textContent = 'You WIN!' :
+    player === 'scissors' && computer === 'rock' ? results.textContent = 'You lose!' :
+    player === 'scissors' && computer === 'paper' ? results.textContent = 'You WIN!' :
+    player === 'paper' && computer === 'scissors' ? results.textContent = 'You lose!' :
+    player === 'paper' && computer === 'rock' ? results.textContent = 'You WIN!' :
+    player === computer ? results.textContent = 'Draw!' : 
+    results.textContent = 'Please choose a valid weapon.';
 
+    results.textContent === 'You WIN!' ? playerScore++ : 
+    results.textContent === 'You lose!' ? computerScore++ : null;
 
-function game() {
-    let counter = 0;
-    for (let i = 0; i < 5; i++) {
-        let result = playRound(prompt('Choose your weapon:'), getComputerChoice());
-        console.log(result);
-        result === 'You WIN!' ? counter++ : result === 'Draw!' ? i-- : null;
-    }
-    console.log(counter > 2 ? 'You are the WINNER!' : 'You are the LOSER!');
+    computerPoint.textContent = computerScore;
+    playerPoint.textContent = playerScore;
+
+    if (playerScore === 5) {
+        results.textContent = 'YOU WON THE GAME!';
+        playerScore = 0;
+        computerScore = 0;
+    } else if (computerScore === 5) {
+        results.textContent = 'You have lost this game.';
+        playerScore = 0;
+        computerScore = 0;
+    };
+
 };
 
-game();
+// function game() {
+//     let counter = 0;
+//     for (let i = 0; i < 5; i++) {
+//         let result = playRound(prompt('Choose your weapon:'), getComputerChoice());
+//         console.log(result);
+//         result === 'You WIN!' ? counter++ : result === 'Draw!' ? i-- : null;
+//     }
+//     console.log(counter > 2 ? 'You are the WINNER!' : 'You are the LOSER!');
+// };
+
+// game();
+
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+rock.addEventListener('click', () => {playRound('rock', getComputerChoice())});
+paper.addEventListener('click', () => {playRound('paper', getComputerChoice())});
+scissors.addEventListener('click', () => {playRound('scissors', getComputerChoice())});
+
